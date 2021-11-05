7304b3934e2497b3b709078ab58bc1fa0f1d6611
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-11-05 12:58:38 +0300

    security of admin panel

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -34,6 +34,11 @@ class Admin implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -108,5 +113,22 @@ class Admin implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->username;
     }
 }
